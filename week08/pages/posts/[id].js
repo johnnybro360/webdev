b0b4283322a16be5360e32d68512d7ae1c6e092c
johnnybro360 <abest82@gmail.com>
2023-10-03 13:07:28 +0800

    update dynamic routes 1

--- week08/pages/posts/[id].js
+++ week08/pages/posts/[id].js
@@ -1,5 +1,9 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
 import {getAllPostIds, getPostData} from '../../lib/posts';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+
 
 // export default function Post() {
 //     return <Layout>...</Layout>;
@@ -16,7 +20,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({params}) {
     // Fetch necessary data for the blog post using params.id
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
@@ -27,11 +31,18 @@ export async function getStaticProps({params}) {
 export default function Post({postData}) {
     return (
         <Layout>
-            {postData.title}
-            <br/>
-            {postData.id}
-            <br/>
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>
+                    {postData.title}
+                </h1>
+                <div className="utilStyles lightText">
+                    <Date dateString={postData.date}/>
+                </div>
+                <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+            </article>
         </Layout>
     );
 }
